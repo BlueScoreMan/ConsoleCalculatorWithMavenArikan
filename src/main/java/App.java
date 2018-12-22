5d61f38fc0a56bc147e3a4698e87f54dd7670d4d
Ufuk Arikan <ufuk.arikan@ruggimial.ch>
2018-12-22 15:45:08 +0100

    Implement main in App and fix pom.xml so it has a manifest

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,20 @@
 public class App {
+
+    private static Integer parseInput(String arg) {
+        try {
+            return Integer.parseInt(arg);
+        } catch (NumberFormatException e) {
+            System.err.println("Two args necessary");
+            throw new RuntimeException();
+        }
+    }
+
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        int valueA = 0;
-        int valueB = 0;
+        if (args.length < 2) {
+            System.err.println("Two args needed");
+            throw new RuntimeException();
+        }
+        System.out.println("Sum of " + parseInput(args[0]) + " + " + parseInput(args[1]) + " = " + calculator.addition(parseInput(args[0]), parseInput(args[1])));
     }
 }
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = {"1", "2"};
+        App.main(args);
+    }
+}
